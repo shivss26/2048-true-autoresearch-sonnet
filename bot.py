c0c6e292c2cedf5ada24fb9f6ed69c0fa395c823
Shivam <shivam.salgaocar.22042@iitgoa.ac.in>
2026-03-26 13:00:30 +0530

    [DISCARD] revert: reduce board quality weight: 0.3 -> 0.2, increase continuation: 0.5 -> 0.6

--- bot.py
+++ bot.py
@@ -61,8 +61,8 @@ def choose_move(board, score):
         # Immediate reward
         value = score_gained
 
-        # Board quality (reduced weight to prioritize score-seeking over structure)
-        value += evaluate_board(new_board) * 0.2
+        # Board quality
+        value += evaluate_board(new_board) * 0.3
 
         # 2-ply lookahead: for each next move, check best third move
         next_valid = get_valid_moves(new_board)
@@ -73,7 +73,7 @@ def choose_move(board, score):
                 next_board, next_score, _ = move(new_board, next_dir)
 
                 # Value of next move + board quality
-                cont_value = next_score + evaluate_board(next_board) * 0.2
+                cont_value = next_score + evaluate_board(next_board) * 0.3
 
                 # Best greedy move from the next board (3rd ply)
                 next_next_valid = get_valid_moves(next_board)
@@ -81,12 +81,12 @@ def choose_move(board, score):
                     best_third = 0
                     for third_dir in next_next_valid:
                         third_board, third_score, _ = move(next_board, third_dir)
-                        best_third = max(best_third, third_score + evaluate_board(third_board) * 0.2)
-                    cont_value += best_third * 0.6
+                        best_third = max(best_third, third_score + evaluate_board(third_board) * 0.3)
+                    cont_value += best_third * 0.5
 
                 best_continuation = max(best_continuation, cont_value)
 
-            value += best_continuation * 0.6
+            value += best_continuation * 0.5
 
         if value > best_value:
             best_value = value
